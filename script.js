b2eaf990e4e995b602bfb267a19782d9e9583c7e
加能 侑生子 <38373129+YukoKano@users.noreply.github.com>
2020-06-27 23:55:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let mic;
+let alpha = 100;
 
-// 全体の初期化（最初に一回だけ呼ばれる）
 function setup() {
   // キャンバスをつくる
   createCanvas(windowWidth, windowHeight);
@@ -13,23 +13,20 @@ function setup() {
     mic = new p5.AudioIn();
     mic.start();
     
-    userStartAudio();
+    userStartAudio();  //これだいじ
 }
 
 // 計算と表示
 function draw() {
   // 背景をぬりつぶす
   background(0);
+
+    let vol = mic.getLevel() * 20; //0 - 20
+    alpha -= vol - 0.3;
+
+    fill(255, alpha);
+    ellipse(width / 2, height / 2, 50, 50);
     
-//  ellipse(100, 100, 100);
-    
-    let vol = mic.getLevel();
-//    console.log(vol);
-    fill(255);
-    
-  let h = map(vol, 0, 1, height, 0);
-    
-    ellipse(width / 2, h, 50, 50);
 }
 
 //http://wgg.hatenablog.jp/entry/20181225/1545703427
