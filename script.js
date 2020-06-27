aeab9e4e5aaff0bd5743e8ee779d9a22633e3295
加能 侑生子 <38373129+YukoKano@users.noreply.github.com>
2020-06-27 16:29:44 +0900

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+// 全体の初期化（最初に一回だけ呼ばれる）
+function setup() {
+  // キャンバスをつくる
+  createCanvas(windowWidth, windowHeight);
+    
+  window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
+  window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
+}
+
+// 計算と表示
+function draw() {
+  // 背景をぬりつぶす
+  background(0);
+
+}
