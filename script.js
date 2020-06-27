e744e45488b99ee92c5b69b5f912207d44ced442
加能 侑生子 <38373129+YukoKano@users.noreply.github.com>
2020-06-27 16:44:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let t = 150;
-
 // 全体の初期化（最初に一回だけ呼ばれる）
 function setup() {
   // キャンバスをつくる
@@ -15,18 +13,6 @@ function draw() {
   background(0);
     
   ellipse(100, 100, 100);
-  
-//  for(j = 0; j < 40; j++){
-//    var seed = (j - frameCount) * 0.02;
-//    var pre_y = noise(seed) * t - t/2 + height/4 * sin(0) + height / 2;
-//    var c = color(noise(seed) * 255,noise(seed + 1) * 255,noise(seed + 2) * 255); 
-//    stroke(c);
-//    for(i = 0;i < w;i+=3){
-//      var y = noise(seed + 0.01 * (i + 1)) * t - t/2 + h/4 * sin(TWO_PI/360*i * 0.8) + h / 2;
-//      line(i, pre_y, i + 3, y);
-//      pre_y = y;
-//    }
-//  }
 
 }
 
