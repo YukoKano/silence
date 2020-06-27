11aa67b349500edb98e5175414c3c7b81b83e054
加能 侑生子 <38373129+YukoKano@users.noreply.github.com>
2020-06-27 23:19:22 +0900

    micTest

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let mic;
+
 // 全体の初期化（最初に一回だけ呼ばれる）
 function setup() {
   // キャンバスをつくる
@@ -5,6 +7,11 @@ function setup() {
     
   window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
   window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
+    
+    noStroke();
+    
+    mic = new p5.AudioIn();
+    mic.start();
 }
 
 // 計算と表示
@@ -12,8 +19,14 @@ function draw() {
   // 背景をぬりつぶす
   background(0);
     
-  ellipse(100, 100, 100);
-
+//  ellipse(100, 100, 100);
+    
+    let vol = mic.getLevel();
+    fill(255);
+    
+  let h = map(vol, 0, 1, 0, 100);
+    
+    ellipse(width / 2, h - 25, 50);
 }
 
 //http://wgg.hatenablog.jp/entry/20181225/1545703427
