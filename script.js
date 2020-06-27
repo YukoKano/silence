c0accf780bf75de03e7e60e674010d43ea18ed9b
加能 侑生子 <38373129+YukoKano@users.noreply.github.com>
2020-06-27 16:35:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ function setup() {
 function draw() {
   // 背景をぬりつぶす
   background(0);
+  
+  for(j = 0; j < 40; j++){
+    var seed = (j - frameCount) * 0.02;
+    var pre_y = noise(seed) * t - t/2 + h/4 * sin(0) + h / 2;
+    var c = color(noise(seed) * 255,noise(seed + 1) * 255,noise(seed + 2) * 255); 
+    stroke(c);
+    for(i = 0;i < w;i+=3){
+      var y = noise(seed + 0.01 * (i + 1)) * t - t/2 + h/4 * sin(TWO_PI/360*i * 0.8) + h / 2;
+      line(i, pre_y, i + 3, y);
+      pre_y = y;
+    }
+  }
 
 }
